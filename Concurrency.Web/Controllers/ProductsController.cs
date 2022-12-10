1268c7642f2f3846a21bcb87c67dd99093cb2903
MuratDuroglu <muratduroglu@icloud.com>
2022-12-10 09:52:45 +0300

    concurrenecy

---
+++ Concurrency.Web/Controllers/ProductsController.cs
@@ -0,0 +1,47 @@
+﻿using Concurrency.Web.DataAccessLayer;
+using Concurrency.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Immutable;
+
+namespace Concurrency.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+
+        private readonly AppDbContext _context;
+
+        public ProductsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult>List()
+        {
+
+            return View( await _context.Products.ToListAsync());  
+        }
+
+
+        public async Task<IActionResult> Update(int Id)
+        {
+
+            var products = await _context.Products.FindAsync(Id);
+            return View(products);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(Product product)
+        {
+
+              _context.Products.Update(product);
+          await  _context.SaveChangesAsync(); 
+            return RedirectToAction(nameof(List));
+
+        }
+
+
+
+
+    }
+}
